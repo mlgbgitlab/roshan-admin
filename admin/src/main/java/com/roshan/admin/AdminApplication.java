c500f07de297e2719fd79c58253c39076bc622f2
mlgbgitlab <297258160@qq.com>
2022-11-01 15:02:05 +0800

    initi2

--- admin/src/main/java/com/roshan/admin/AdminApplication.java
+++ admin/src/main/java/com/roshan/admin/AdminApplication.java
@@ -1,8 +1,9 @@
 package com.roshan.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableAdminServer
 @SpringBootApplication
 public class AdminApplication {
 
